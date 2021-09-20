e8139fb41187b0143c2be248c2f6a5b30cb61d84
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-09-20 13:08:17 +0530

    Refactor problem1 with promises

--- problem1.js
+++ problem1.js
@@ -4,9 +4,9 @@
 //     2. Delete those files simultaneously
 // 
 
-const FS = require('fs');
+const FS = require('fs').promises;
 
-module.exports.problem1 = (DIR_NAME, callback, NUMBER_OF_FILES = 10) => {
+module.exports.problem1 = (DIR_NAME, NUMBER_OF_FILES = 10) => {
     function randomDataGenerator(dataLength) {
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
         charLength = characters.length;
@@ -22,26 +22,28 @@ module.exports.problem1 = (DIR_NAME, callback, NUMBER_OF_FILES = 10) => {
     const FILE_NAMES = []
 
     for(dataSetIndex = 0; dataSetIndex < NUMBER_OF_FILES; dataSetIndex += 1){
-        DATASETS.push(randomDataGenerator(100));
+        LETTERS_TO_GENERATE = Math.floor(Math.random() * 1000000)
+        DATASETS.push(randomDataGenerator(LETTERS_TO_GENERATE));
         FILE_NAMES.push(randomDataGenerator(5));
     }
-
-    if(!FS.existsSync(`${__dirname}/${DIR_NAME}`)) {
-        FS.mkdir(`${__dirname}/${DIR_NAME}`, (error) => {
-            if(error) {
-                console.log(error);
-            } else {
     
-                for(run = 0; run < NUMBER_OF_FILES; run += 1) {
-                    FILE_PATH = `${__dirname}/${DIR_NAME}/${FILE_NAMES[run]}.json`;
-                    callback(FILE_PATH, DATASETS[run]);
-                }
-            }
-        });
-    } else {
-        for(run = 0; run < NUMBER_OF_FILES; run += 1) {
-            FILE_PATH = `${__dirname}/${DIR_NAME}/${FILE_NAMES[run]}.json`;
-            callback(FILE_PATH, DATASETS[run]);
-        }
-    }
+    const DIR_PATH = `${__dirname}/${DIR_NAME}`;
+
+    FS.mkdir(DIR_PATH)
+        .then(() => console.log(`${DIR_PATH} created\n\n`))
+        .then(() => 
+            Promise.all(FILE_NAMES.map(async (FILE_NAME, run) => {
+                const FILE_PATH = `${DIR_PATH}/${FILE_NAME}.json`;
+
+                return await FS.writeFile(FILE_PATH, DATASETS[run])
+                    .then(() => console.log(`${FILE_PATH} created`))
+                    .then(() => FS.unlink(FILE_PATH))
+                    .then(() => console.log(`${FILE_PATH} deleted`))
+                    .catch((error) => console.log(error));
+            }))
+        )
+        .then(() => console.log('\n\nFiles created and deleted. Deleting the directory.'))
+        .then(() => FS.rmdir(DIR_PATH))
+        .then(() => console.log(`${DIR_PATH} deleted`))
+        .catch((error) => console.log(error));
 }
